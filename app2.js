a58658dfd73db74ee2c6b38fc05717769fd67864
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-08-14 23:16:53 +0200

    solve error asynchonia

--- app2.js
+++ app2.js
@@ -4,18 +4,36 @@
 // async-await.
 
 
-const runTimeOut = () => {
-const promise = new Promise((resolve) => {
-setTimeout(function () {
-resolve();
-}, 2000);
-})
-
+const runTimeOut2 = () => {
+    const promise = new Promise((resolve) => {
+        setTimeout(function () {
+            resolve();
+        }, 2000);
+    })
     promise.then(() => {console.log('Time out!')})
 
-};
+    };
 
-runTimeOut();
+runTimeOut2();
+
+
+// -------------
+
+function runTimeOut(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    
+    console.log('time star')
+    
+    await runTimeOut(2000); 
+    console.log('Time out 2!');
+}
+
+demo();
+
+
 
 // 2.2 Convierte la siguiente función con un fetch utilizando async-await.
 // Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
