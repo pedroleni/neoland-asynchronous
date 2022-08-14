40b617b399b53b09438f152d8008ea4e457f36af
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-08-14 22:29:50 +0200

    ok

---
+++ app2.js
@@ -0,0 +1,37 @@
+// **Iteración #2: async-await**
+
+// 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando
+// async-await.
+
+
+const runTimeOut = () => {
+const promise = new Promise((resolve) => {
+setTimeout(function () {
+resolve();
+}, 2000);
+})
+
+    promise.then(() => {console.log('Time out!')})
+
+};
+
+runTimeOut();
+
+// 2.2 Convierte la siguiente función con un fetch utilizando async-await.
+// Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
+
+
+function getCharacters () {
+fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => console.log(characters));
+}
+
+getCharacters();
+
+
+// --- solucion 
+
+const getCharacters2 = async () => {
+    const result = await fetch('https://rickandmortyapi.com/api/character');
+    const resultToJson = await result.json();
+    return resultToJson;
+}
